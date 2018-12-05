8b196deea16af40706cdb8b5fae99822e17717b1
Fabian Stäber <fabian@fstab.de>
2018-12-05 17:43:58 +0100

    change column width

--- src/main/java/de/fstab/helloworld/HelloWorldApplication.java
+++ src/main/java/de/fstab/helloworld/HelloWorldApplication.java
@@ -22,19 +22,28 @@ public class HelloWorldApplication {
         result = result + "ENVIRONMENT\n";
         result = result + "-----------\n\n";
         for (var e : new TreeMap<>(System.getenv()).entrySet()) {
-            result = result + "| " + fixedLength(20, e.getKey()) + " | " + fixedLength(50, e.getValue()) + " |\n";
+            // key length 30 so that KUBERNETES_SERVICE_PORT_HTTPS fits in
+            // value length 42 so that the overall table fits in an 80 char terminal window
+            result = result + String.format("| %-30s | %-42s |\n", stripAndTruncate(30, e.getKey()), stripAndTruncate(42, e.getValue()));
         }
         return result;
     }
 
-    private String fixedLength(int length, String s) {
+    private String stripAndTruncate(int length, String s) {
+        return truncate(length, stripNewlinesAndTabs(s));
+    }
+
+    private String stripNewlinesAndTabs(String s) {
         if (s == null) {
-            s = "null";
+            return s;
         }
-        s = s.replaceAll("\\s+", " "); // replace newlines and tabs with spaces
-        if (s.length() > length) {
-            s = s.substring(0, length - 3) + "..."; // truncate string
+        return s.replaceAll("\\s+", " ");
+    }
+
+    private String truncate(int length, String s) {
+        if (s != null && s.length() > length) {
+            return s.substring(0, length - 3) + "...";
         }
-        return String.format("%-" + length + "s", s); // make n chars long, padded with spaces.
+        return s;
     }
 }
